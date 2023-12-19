c4efac2b8ac7c8a958bec33866e45d85e1630952
Swapnil <rupakheteeswapnil@gmail.com>
2023-12-20 00:07:31 +0545

    navbar-design-complete

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import './Navbar.css'
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoLogoGitlab } from "react-icons/io5";
 
 const Navbar = () => {
   return (
     <div className="navbar-container">
         <div className="navbar-items">
-            <div className="navbar-left">LOGO</div>
+            <div className="navbar-left"><IoLogoGitlab className='logo' /></div>
             <div className="navbar-right">
                 <span className='buttons'>LOGIN</span>
-                <span className='burger'>Hamburger</span>
+                <span className='burger'><RxHamburgerMenu className='burger-img'/></span>
                 </div>
         </div>
     </div>
